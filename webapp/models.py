30c7ee31a286892c2b737ddb9a662d98faa3a49c
Ankit Sharma <anksharma90@gmail.com>
2014-04-13 02:06:11 -0400

    Added User model

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    ROLE_CHOICES = (
+                    ('Doctor','Doctor'),
+                    ('Nurse','Nurse'),
+                    ('Management','Management'),
+                    ('Researcher','Researcher'),
+                    )
+    user_name = models.CharField(max_length=50, primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
