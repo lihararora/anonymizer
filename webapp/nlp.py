c71d12091d26bb170ece7254c02ea98f171f690e
lihararora <lihararora@gmail.com>
2014-04-30 13:02:02 -0400

    Sensitivity set to medium

--- webapp/nlp.py
+++ webapp/nlp.py
@@ -4,11 +4,11 @@ from nltk import sent_tokenize, word_tokenize, pos_tag, ne_chunk
 from pprint import pprint
 
 class Entity(object):
-    def __init__(self, name, type, sensitivity):
+    def __init__(self, name, type):
         self.name = name
         self.type = type
         self.id = 0
-        self.sensitivity = sensitivity
+        self.sensitivity = "Medium"
 
 def get_names(text):
     tokens = nltk.tokenize.word_tokenize(text)
@@ -30,7 +30,7 @@ def get_names(text):
                 for part in person:
                     name += part + ' '
                 #if name[:-1] not in person_list:
-                entities.append(Entity(name[:-1], "Person", 0))
+                entities.append(Entity(name[:-1], "Person"))
                 person_list.append(name[:-1])
                 name = ''
         person = []
@@ -41,7 +41,7 @@ def get_names(text):
                 for part in organization:
                     name += part + ' '
                 #if name[:-1] not in organization_list:
-                entities.append(Entity(name[:-1], "Organization", 0))
+                entities.append(Entity(name[:-1], "Organization"))
                 organization_list.append(name[:-1])
                 name = ''
         organization = []
@@ -52,7 +52,7 @@ def get_names(text):
                 for part in gpe:
                     name += part + ' '
                 #if name[:-1] not in gpe_list:
-                entities.append(Entity(name[:-1], "Geo Entity", 0))
+                entities.append(Entity(name[:-1], "Geo Entity"))
                 gpe_list.append(name[:-1])
                 name = ''
         gpe = []
@@ -60,27 +60,27 @@ def get_names(text):
     dates = []
     dates = re.findall(r'(\d{2}[/.-]\d{2}[/.-]\d{4})', text)
     for date in dates:
-        entities.append(Entity(date, "Date", 0))
+        entities.append(Entity(date, "Date"))
     dates = []
     dates = re.findall(r'(\d{4}[/.-]\d{2}[/.-]\d{2})', text)
     for date in dates:
-        entities.append(Entity(date, "Date", 0))
+        entities.append(Entity(date, "Date"))
     dates = []
     dates = re.findall(r'\d\d\s(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-zA-Z]{0,6}\s\d{4}|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-zA-Z]{0,6}\s\d{2,4}', text, re.IGNORECASE)
     for date in dates:
-        entities.append(Entity(date, "Date", 0))
+        entities.append(Entity(date, "Date"))
     times = []
     times = re.findall(r'([0-2][0-9]:[0-5]\d)', text)
     for time in times:
-        entities.append(Entity(time, "Time", 0))
+        entities.append(Entity(time, "Time"))
     phones = []
     phones = re.findall(r'([0-9]{3}-[0-9]{3}-[0-9]{4})', text)
     for phone in phones:
-        entities.append(Entity(phone, "Phone", 0))  
+        entities.append(Entity(phone, "Phone"))  
     emails = []
     emails = re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]', text)
     for email in emails:
-        entities.append(Entity(email, "Email Id", 0))  
+        entities.append(Entity(email, "Email Id"))  
     
     for entity in entities:
         offset = text.find(entity.name)
